5cd387a47e7ed98375afa0bcb8fdfe82e0a4c9d1
Koech <koech@novek.io>
2024-01-13 10:15:08 +0300

    (2.DissectingQMLSyntax): updated 5-PropertyHandlers: cmake, formatting, and QML

--- 2.DissectingQmlSyntax/5-PropertyHandlers/main.cpp
+++ 2.DissectingQmlSyntax/5-PropertyHandlers/main.cpp
@@ -7,15 +7,17 @@
 
 int main(int argc, char *argv[])
 {
-	
-	QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("5-PropertyHandlers", "Main");
+  QGuiApplication app(argc, argv);
 
-    return app.exec();
+  QQmlApplicationEngine engine;
+  QObject::connect(
+    &engine,
+    &QQmlApplicationEngine::objectCreationFailed,
+    &app,
+    []() { QCoreApplication::exit(-1); },
+    Qt::QueuedConnection);
+  engine.loadFromModule("5-PropertyHandlers", "Main");
 
+  return app.exec();
 }
