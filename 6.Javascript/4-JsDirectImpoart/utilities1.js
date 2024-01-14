7645eee0a6e26f3dc85778729de96121ecbec3c0
Koech <koech@novek.io>
2024-01-14 11:12:10 +0300

    (6.Javascript): updated and formatted 4-JsDirectImpoart/Main.qml and utilities.js

--- 6.Javascript/4-JsDirectImpoart/utilities1.js
+++ 6.Javascript/4-JsDirectImpoart/utilities1.js
@@ -1,6 +1,9 @@
 // Copyright (c) Daniel Gakwaya.
 // SPDX-License-Identifier: MIT
 
+/*
+Resource: https://doc.qt.io/qt-6/qtqml-javascript-resources.html
+*/
 function greeting()
 {
     console.log("Hello there from external JS file utilities1.js")
